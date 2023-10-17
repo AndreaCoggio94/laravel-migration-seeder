c06abb59126335708371ce36dc7c06bc92e064c7
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-10-17 17:41:10 +0200

    added columns to fill up to seeder

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Faker\Generator as Faker;
+use App\Models\Train;
 class TrainsTableSeeder extends Seeder
 {
     /**
@@ -12,8 +15,41 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i=0; $i < 20; $i++){
+
+        $train = new Train();
+
+        // company
+        $train->company = $faker->words(3,true);
+        // places
+        $train->station_from = $faker->word();
+        $train->station_to = $faker->word();
+        // time
+        $train->departure = $faker->word();
+        $train->arrival = $faker->word();
+        // alphanumeric
+        $train->code = $faker->word();
+        // small int
+        $train->number_of_carriage = $faker->word();
+        // boolean
+        $train->running_on_time = $faker->word();
+        $train->cancelled = $faker->word();
+        // date
+        $train->date_departure = $faker->word();
+        $train->date_arrival = $faker->word();
+
+        // examples
+
+        // $train->genre = $faker->word();
+        // $train->plot = $faker->paragraph();
+        // $train->publishing_year = $faker->year();
+        // $train->pages = $faker->numberBetween(30,999);
+        // $train->price = $faker->randomFloat(2, 5, 70);
+
+
+        $train->save();
+}
     }
 }
